15eb4f9d247ebf6587726b51cd6a6ea2c93fea90
ThomasSwinicki <tomswinicki@yahoo.com>
2016-12-28 23:03:14 -0500

    Update
    Added initial JTextLabel for grades.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,16 @@
 import java.awt.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 
 public class Calculator extends JFrame{
 
-	private JLabel item1;
-	private JTextField grades;
-	private JTextField percents;
+	private JLabel grade_lbl;
 	
 	public Calculator(){
 		initUI();
@@ -22,8 +27,46 @@ public class Calculator extends JFrame{
 		JLabel descript = new JLabel("Please enter your currents grades and their respecitve" 
 				+ " weight percentages.");
 		
-		createLayout(descript);
+		JTextField grade1 = new JTextField(5);
+		JLabel grade_lbl = new JLabel();
+		
+		
+		createLayout(descript,grade1, grade_lbl);
+		
 	}
+	
+	private class MyDocumentListener implements DocumentListener {
+
+        private String text;
+
+        public void insertUpdate(DocumentEvent e) {
+            updateLabel(e);
+        }
+
+        
+        public void removeUpdate(DocumentEvent e) {
+            updateLabel(e);
+        }
+
+       
+        public void changedUpdate(DocumentEvent e) {
+        }
+
+        private void updateLabel(DocumentEvent e) {
+
+            Document doc = e.getDocument();
+            int len = doc.getLength();
+
+            try {
+                text = doc.getText(0, len);
+            } catch (BadLocationException ex) {
+                Logger.getLogger(Calculator.class.getName()).log(
+                        Level.SEVERE, null, ex);
+            }
+            grade_lbl.setText(text);
+
+        }
+    }
 	
 	public static void main(String[] args){
 		EventQueue.invokeLater(() -> {
@@ -39,9 +82,18 @@ public class Calculator extends JFrame{
 		
 		g1.setAutoCreateContainerGaps(true);
 		
-		g1.setHorizontalGroup(g1.createSequentialGroup().addComponent(arg[0]));
+		g1.setHorizontalGroup(g1.createSequentialGroup().addComponent(arg[0]).addComponent(arg[1])
+                .addGap(250));
 		
-		g1.setVerticalGroup(g1.createSequentialGroup().addComponent(arg[0]));
+		g1.setVerticalGroup(g1.createSequentialGroup().addComponent(arg[0], GroupLayout.DEFAULT_SIZE, 
+                GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+        .addComponent(arg[1])
+        .addGap(150));
+		
+		pack();
 	}
+	
+
+    
 	
 }
