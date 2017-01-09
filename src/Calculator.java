8869b28019cded88fe7fee337d0a3a25213b0924
ThomasSwinicki <tomswinicki@yahoo.com>
2017-01-08 22:40:20 -0500

    update

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,24 @@
 //Thomas Swinicki
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 
 import net.miginfocom.swing.MigLayout;
 
-public class Calculator extends JFrame{
+public class Calculator extends JFrame implements ActionListener{
+	
+	private JTextField grade1;
+	private JTextField percent1;
+	private JLabel finalGrade;
 	
 	public Calculator(){
 		initUI();
@@ -30,6 +43,11 @@ public class Calculator extends JFrame{
 		//add text field where the user will input grades and percentages
 		JTextField grade1 = new JTextField(5);
 		JTextField percent1 = new JTextField(5);
+		
+		//DocumentListeners are created so that the JLabel for the final grade can be 
+		//updated using the changes from the grade and percentage text fields
+		grade1.getDocument().addDocumentListener(new MyDocumentListener());
+		percent1.getDocument().addDocumentListener(new MyDocumentListener());
 
 		//labels for where the grades and percentages go
 		JLabel grades =  new JLabel("Grades");
@@ -37,9 +55,62 @@ public class Calculator extends JFrame{
 		JLabel percentages = new JLabel("Percentage");
 		
 		JButton calculate = new JButton("Calculate");
+		calculate.addActionListener(this);
+		
+		JLabel finalGrade = new JLabel("test");
 		
 		//create layout for the GUI
-		createLayout(descript, grades, grade1, percentages, percent1, calculate);
+		createLayout(descript, grades, grade1, percentages, percent1, calculate, finalGrade);
+	
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e){
+		/*int g1 = Integer.parseInt(grade1.getText());
+		double p1 = (double) Integer.parseInt(percent1.getText());
+		double fGrade = g1 * p1;
+		String result = String.valueOf(fGrade);*/
+		JOptionPane.showMessageDialog(this, grade1.getText());
+		//finalGrade.setText(result);
+	}
+	
+	private class MyDocumentListener implements DocumentListener{
+
+		private String text;
+		
+		@Override
+		public void changedUpdate(DocumentEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void insertUpdate(DocumentEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void removeUpdate(DocumentEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		private void updateLabel(DocumentEvent e){
+			Document doc = e.getDocument();
+			int len = doc.getLength();
+			
+			try{
+				text = doc.getText(0, len);
+			}
+			catch (BadLocationException ex){
+				Logger.getLogger(Calculator.class.getName()).log(Level.SEVERE, null, ex);
+			}
+			
+			if(!grade1.getText().equals(null) && !percent1.getText().equals(null)){
+				finalGrade.setText(text);
+			}
+		}
 		
 	}
 	
@@ -49,6 +120,7 @@ public class Calculator extends JFrame{
     		calc.setVisible(true);
         });
 	}
+	
 	
 	//may consider using a different layout for program/need to learn more about GroupLayout
 	/*public void createLayout(JComponent... arg) {
@@ -102,14 +174,10 @@ public class Calculator extends JFrame{
 		pane.add(arg[3], "gapright 300");
 		pane.add(arg[2], "gapleft 300");
 		pane.add(arg[4], "gapright 300");
+		pane.add(arg[6], "wrap");
 		pane.add(arg[5], "align right");
 		
 		pack();
 	}
-	
-	
-	
-
-    
 	
 }
