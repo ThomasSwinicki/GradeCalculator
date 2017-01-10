c8ff20849f6fc3a4319485d03e3b856aabc31538
ThomasSwinicki <tomswinicki@yahoo.com>
2017-01-09 19:46:30 -0500

    Update
    Fixed NullPointerException error for JTextField

--- src/Calculator.java
+++ src/Calculator.java
@@ -41,13 +41,15 @@ public class Calculator extends JFrame implements ActionListener{
 				+ " weight percentages.");
 		
 		//add text field where the user will input grades and percentages
-		JTextField grade1 = new JTextField(5);
-		JTextField percent1 = new JTextField(5);
+		grade1 = new JTextField(5);
+		percent1 = new JTextField(5);
 		
 		//DocumentListeners are created so that the JLabel for the final grade can be 
 		//updated using the changes from the grade and percentage text fields
-		grade1.getDocument().addDocumentListener(new MyDocumentListener());
-		percent1.getDocument().addDocumentListener(new MyDocumentListener());
+		
+		//try without documentlisteners, it should work, need to fix null pointer exception
+		//grade1.getDocument().addDocumentListener(new MyDocumentListener());
+		//percent1.getDocument().addDocumentListener(new MyDocumentListener());
 
 		//labels for where the grades and percentages go
 		JLabel grades =  new JLabel("Grades");
@@ -57,7 +59,7 @@ public class Calculator extends JFrame implements ActionListener{
 		JButton calculate = new JButton("Calculate");
 		calculate.addActionListener(this);
 		
-		JLabel finalGrade = new JLabel("test");
+		finalGrade = new JLabel("test");
 		
 		//create layout for the GUI
 		createLayout(descript, grades, grade1, percentages, percent1, calculate, finalGrade);
@@ -66,55 +68,15 @@ public class Calculator extends JFrame implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e){
-		/*int g1 = Integer.parseInt(grade1.getText());
+		
+		int g1 = Integer.parseInt(grade1.getText());
 		double p1 = (double) Integer.parseInt(percent1.getText());
 		double fGrade = g1 * p1;
-		String result = String.valueOf(fGrade);*/
-		JOptionPane.showMessageDialog(this, grade1.getText());
-		//finalGrade.setText(result);
+		String result = String.valueOf(fGrade);
+		finalGrade.setText(result);
 	}
 	
-	private class MyDocumentListener implements DocumentListener{
-
-		private String text;
-		
-		@Override
-		public void changedUpdate(DocumentEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void insertUpdate(DocumentEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void removeUpdate(DocumentEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		private void updateLabel(DocumentEvent e){
-			Document doc = e.getDocument();
-			int len = doc.getLength();
-			
-			try{
-				text = doc.getText(0, len);
-			}
-			catch (BadLocationException ex){
-				Logger.getLogger(Calculator.class.getName()).log(Level.SEVERE, null, ex);
-			}
-			
-			if(!grade1.getText().equals(null) && !percent1.getText().equals(null)){
-				finalGrade.setText(text);
-			}
-		}
-		
-	}
-	
-	public static void main(String[] args){
+    public static void main(String[] args){
 		EventQueue.invokeLater(() -> {
             Calculator calc = new Calculator();
     		calc.setVisible(true);
